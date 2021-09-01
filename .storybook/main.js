aa6611902ac61713f3f2ad68e0d30fc7d5a7b92e
Rasheed <rasheed.shaik@shell.com>
2021-09-01 15:11:20 +0530

    storybook config

---
+++ .storybook/main.js
@@ -0,0 +1,43 @@
+module.exports = {
+    stories: ["../src/**/*.stories.tsx"],
+    addons: [
+      "@storybook/addon-actions",
+      "@storybook/addon-links",
+      "@storybook/addon-essentials",
+      "@storybook/addon-controls",  
+    ],
+    typescript: {
+      check: false,
+      checkOptions: {},
+      reactDocgen: 'react-docgen-typescript',
+      reactDocgenTypescriptOptions: {
+        shouldExtractLiteralValuesFromEnum: true,
+        propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+        compilerOptions: {
+          allowSyntheticDefaultImports: true,
+        }
+      },
+    },
+    webpackFinal: async (config) => {
+      // Replaces the webpack rule that loads SVGs as static files to leave out SVG files for us to handle
+      const indexOfRuleToRemove = config.module.rules.findIndex(rule => rule.test.toString().includes('svg'))
+      config.module.rules.splice(indexOfRuleToRemove, 1, {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/,
+        loader: require.resolve('file-loader'),
+        options: {
+          name: 'static/media/[name].[hash:8].[ext]',
+          esModule: false
+        }
+      })
+  
+      config.module.rules.push(
+        {
+          test: /.svg$/,
+          use: ['@svgr/webpack'],
+        },
+  
+      )
+      config.resolve.extensions.push('.ts', '.tsx');
+      return config
+    },
+  };
