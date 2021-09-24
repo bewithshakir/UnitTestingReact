0876eb35416b69fa15255cb7f2d69086437c330e
abhijeet.gupta <abhijeet.gupta@abhijeetguptas-MacBook-Pro.local>
2021-09-24 16:49:24 +0530

    Fixed layout and css issues in filter panel and form components

--- src/components/UIComponents/CustomerFilterPanel/CustomerFilterPanel.component.tsx
+++ src/components/UIComponents/CustomerFilterPanel/CustomerFilterPanel.component.tsx
@@ -6,6 +6,7 @@ import { Drawer, IconButton, AppBar, Toolbar, Box, Grid } from "@mui/material";
 
 import Select from "../Select/dropdown";
 import {DatePicker} from "../DatePicker/DatePicker.component";
+import { Button } from '../Button/Button.component';  
 import  {CloseIcon} from "../../../assets/icons";
 import "./CustomerFilterPanel.style.scss";
 
@@ -18,11 +19,9 @@ export const CustomerFilterPanel: React.FC<InfoPanelProps> = ({ open, onClose })
   const [form, setForm] = React.useState<{ state: string, city: string, settlementType:string, startDate: Date | string | null | moment.Moment, endDate: Date | string | null |moment.Moment}>(
     {state:"",city:"",settlementType:"", startDate: '' , endDate: ''}
   )
+  
   const { t, i18n } = useTranslation();
-  const onDateChange =(name: string, newValue: Date| string| null| moment.Moment)=>{
-    setForm(x => ({ ...x, [name]: newValue }));
-  }
-    
+
   const geoData = {
     states : [
       { label: 'Alabama', value: 'Alabama' },
@@ -39,28 +38,32 @@ export const CustomerFilterPanel: React.FC<InfoPanelProps> = ({ open, onClose })
       { label: 'Houston', value: 'Houston' }
     ]
   }
-
+  
   const settlementTypes = [
     { label: 'Invoice', value: 'Invoice' },
     { label: 'Voyager', value: 'Voyager' },
     { label: 'WEX', value: 'WEX' }
   ]
 
+  const onDateChange =(name: string, newValue: Date| string| null| moment.Moment)=>{
+    setForm(x => ({ ...x, [name]: newValue }));
+  }
+
   const handleSelect = (e: any) => {
     setForm(x => ({ ...x, [e.target.name]: e.target.value }));
   }
 
   return (
-      <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={open}
-      >
-    <Drawer
+    <Backdrop
+      sx={{ color: "var(--White)", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={open}
+    >
+      <Drawer
       className="customer_filter_panel"
       variant="permanent"
       anchor="right" 
       open={open}>
-      <div className="customer_filter_panel_header">
+      <div className="cust_filter_panel_header">
         <Box sx={{ flexGrow: 1 }}>
         
           <AppBar position="static" sx={{'backgroundColor':'var(--Blue-Btn)'}}>
@@ -78,9 +81,20 @@ export const CustomerFilterPanel: React.FC<InfoPanelProps> = ({ open, onClose })
           </AppBar>
         </Box>
       </div>
-      <div className="customer_filter_panel_content">
-        {JSON.stringify(form)}
-        <Grid container spacing={2}>
+      <div className="cust_filter_panel_content">
+        {/* {JSON.stringify(form)} */}
+        <Grid container spacing={2} className="cust_filter_parent_grid">
+          <Grid item xs={12} container>
+            <Grid item xs={12} className="cust_filter_date_label_grid">
+              Period
+            </Grid>
+            <Grid item xs={6}>
+              <DatePicker placeholder="From" name="startDate" onChange={onDateChange} value={form.startDate} />
+            </Grid>
+            <Grid item xs={6}>
+              <DatePicker placeholder="To" name="endDate" onChange={onDateChange} value={form.endDate} />
+            </Grid>
+          </Grid>
           <Grid item xs={12}>
             <Select
               name='state'
@@ -116,48 +130,24 @@ export const CustomerFilterPanel: React.FC<InfoPanelProps> = ({ open, onClose })
               multiple
               width="100%"
             />
-          </Grid>
-          <Grid item xs={6}>
-            <DatePicker placeholder="From" name="startDate" onChange={onDateChange} value={form.startDate} />
-          </Grid>
-          <Grid item xs={6}>
-            <DatePicker placeholder="To" name="endDate" onChange={onDateChange} value={form.endDate} />
-          </Grid>
+          </Grid>  
         </Grid>
-        {/* <div className="filter_panel_field_box">
-          <Select
-              name='state'
-              label='Select State'
-              value={form.state}
-              placeholder=''
-              items={geoData.states}
-              onChange={handleSelect}
-              multiple
-          />
+        <div className="cust_filter_buttons_container">
+        < Button
+          types="cancel"
+          aria-label={t("customer-filter-panel.buttons.clear all")}
+          onClick={() => { }}
+        >
+          {t("customer-filter-panel.buttons.clear all")}
+        </Button > 
+        <Button
+          types="save"
+          aria-label={t("customer-filter-panel.buttons.apply")}
+          onClick={() => { }}
+        >
+          {t("customer-filter-panel.buttons.apply")}
+        </Button>
         </div>
-        <div className="filter_panel_field_box">
-          <Select
-            name='city'
-            label='Select City'
-            value={form.city}
-            placeholder=''
-            items={geoData.cities}
-            onChange={handleSelect}
-            multiple
-          />
-        </div>
-        <div className="filter_panel_field_box">
-          <Select
-            name='settlement-type'
-            label='Settlement Type'
-            value={form.settlementType}
-            placeholder=''
-            items={settlementTypes}
-            onChange={handleSelect}
-            multiple
-          />
-        </div> */}
-    
       </div>
     </Drawer>
     </Backdrop>
