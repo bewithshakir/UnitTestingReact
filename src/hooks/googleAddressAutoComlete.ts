5d31cddb5ba3ba2af0890d5ecf71b62fe05cc4dd
Nikhil Patel <nikhil.patel2@shell.com>
2021-10-05 17:16:50 +0530

    eliminate unnecessary api calls for the search api.

--- src/hooks/googleAddressAutoComlete.ts
+++ src/hooks/googleAddressAutoComlete.ts
@@ -4,28 +4,32 @@ import { useQuery } from "react-query";
 
 
 const getAddresses = async (searchQuery: string) => {
-    const payload: AxiosRequestConfig = {
-        method: 'get',
-        url: `http://20.81.30.168:4001/api/customer-service/address/auto-complete?address=${searchQuery}`
+    if (searchQuery) {
+        const payload: AxiosRequestConfig = {
+            method: 'get',
+            url: `http://20.81.30.168:4001/api/customer-service/address/auto-complete?address=${searchQuery}`
+        }
+        const { data } = await axios(payload)
+        return data;
     }
-    const { data } = await axios(payload)
-    return data;
 };
 
-export function FetchGoogleAddress(searchQuery: string) {
+export function FetchGoogleAddress (searchQuery: string) {
     return useQuery(["googleAddress", searchQuery], () => getAddresses(searchQuery));
 }
 
 const getAddress = async (placeId: string) => {
-    const payload: AxiosRequestConfig = {
-        method: 'get',
-        url: `http://20.81.30.168:4001/api/customer-service/address/place-detail?placeId=${placeId}`
+    if (placeId) {
+        const payload: AxiosRequestConfig = {
+            method: 'get',
+            url: `http://20.81.30.168:4001/api/customer-service/address/place-detail?placeId=${placeId}`
+        }
+        const { data } = await axios(payload)
+        return data;
     }
-    const { data } = await axios(payload)
-    return data;
 };
 
-export function FetchFormattedAddress(placeId: string) {
+export function FetchFormattedAddress (placeId: string) {
     return useQuery(["formattedAddress", placeId], () => getAddress(placeId));
 }
 
