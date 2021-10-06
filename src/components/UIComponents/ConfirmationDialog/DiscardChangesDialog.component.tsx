e547806986f10c01ed349e331321e2ea9b93d12e
Nikhil Patel <nikhil.patel2@shell.com>
2021-10-06 21:01:49 +0530

    Add new UI Kit Discard dialoag component

---
+++ src/components/UIComponents/ConfirmationDialog/DiscardChangesDialog.component.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { Button } from '../Button/Button.component';
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Typography } from '@mui/material';
+import PropTypes from 'prop-types'
+import './DiscardChangesDialog.style.scss';
+
+interface Props {
+    title: string;
+    content: string;
+    open: boolean;
+    handleToggle: () => void;
+    handleConfirm: () => void;
+}
+function DiscardChangesDialog (props: Props) {
+    const { title, content, open, handleToggle, handleConfirm } = props;
+
+
+    return (
+        <Dialog
+            open={open}
+            maxWidth="sm"
+            // ---------  un-comment the code to auto close model on outside click ---------
+            // onClose={handleToggle} 
+            aria-labelledby="discard-changes-dialog-title"
+            aria-describedby="discard-changes-dialog-description"
+        >
+            <Typography variant="h2" component="h2" className="fw-bold" p={2.5}>
+                {title}
+            </Typography>
+            <DialogContent
+                sx={{
+                    paddingTop: 0,
+                    paddingLeft: 2.5,
+                    paddingRight: 2.5,
+                }}>
+                <DialogContentText id="discard-changes-dialog-description" color={'var(--Darkgray)'}>
+                    {content}
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions
+                className="content"
+                sx={{
+                    paddingTop: 0,
+                    paddingLeft: 2.5,
+                    paddingRight: 2.5,
+                }}>
+                <Button variant="outlined" onClick={handleToggle} className="action-no">
+                    No
+                </Button>
+                <Button color="default" onClick={handleConfirm} className="action-yes">
+                    Yes
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+DiscardChangesDialog.propTypes = {
+
+}
+
+export default DiscardChangesDialog;
