6c004beb0c02d4b70895d66a17edbf5d41b3e048
Rasheed <rasheed.shaik@shell.com>
2021-10-07 21:42:38 +0530

    Multi-Select Dropdown

--- src/components/UIComponents/DemoComponents/DemoComponents.component.tsx
+++ src/components/UIComponents/DemoComponents/DemoComponents.component.tsx
@@ -11,7 +11,7 @@ import GridComponent from '../DataGird/grid.component';
 import ToastMessage from '../ToastMessage/ToastMessage.component';
 import { Footer } from '../Footer/Footer.component';
 import Input from '../Input/Input';
-import Select from '../Select/SingleSelect';
+import Select from '../Select/MultiSelect';
 import SearchInput from '../SearchInput/SearchInput';
 import GoogleAutoCompleteAddress from '../GoogleAddressComponent/GoogleAutoCompleteAddress';
 import { DatePicker } from '../DatePicker/DatePicker.component';
@@ -42,7 +42,7 @@ export const DemoComponents: React.FC = () => {
     const handleMessageBoxClose = () => {
         setOpen(false);
     }
-    const [form, setForm] = useState({ userName: '', email: '', item: { label: 'Nike', value: 'Nike' }, searchTerm: '', startDate: moment(), endDate: moment(), address:{addressLine1:'', addressLine2:'', state:'', city:'', postalCode:''} });
+    const [form, setForm] = useState({ userName: '', email: '', item: [{ label: 'Nike', value: 'Nike' }], searchTerm: '', startDate: moment(), endDate: moment(), address:{addressLine1:'', addressLine2:'', state:'', city:'', postalCode:''} });
     const debouncedValue = useDebounce<string>(form.searchTerm, 1000);
     const items = [
         { label: 'Amazon', value: 'Amazon23' },
---
+++ src/components/UIComponents/Select/MultiSelect.tsx
@@ -0,0 +1,118 @@
+import { FormikErrors } from 'formik';
+import { Fragment, Children } from 'react';
+import Select, { components, DropdownIndicatorProps, OptionProps, ValueContainerProps } from 'react-select';
+import { FormHelperText, InputLabel, FormControl } from '@mui/material';
+import './SingleSelect.scss';
+import { ArrowDown, CheckedCheckboxIcon as Check, UncheckedCheckboxIcon as UnCheck } from '../../../assets/icons';
+
+
+type item = {
+    label: string,
+    value: string | number
+}
+interface props {
+    id?: string;
+    name?: string;
+    label: string;
+    placeholder?: string;
+    value?: Array<item>;
+    items: Array<item>;
+
+    required?: boolean;
+    error?: boolean;
+    helperText?: string | FormikErrors<item> | undefined;
+
+    description?: string;
+    onChange: (...args: any[]) => void;
+    onBlur?: (...args: any[]) => void;
+}
+
+export default function MultiSelect(props: props) {
+    
+
+    const ValueContainer = (props:ValueContainerProps<any>) => {
+        const { getValue, hasValue, children } = props;
+        const value = getValue();
+        const length = value.length;
+        const abLength = length - 2 ;
+
+        return(
+            <components.ValueContainer {...props}>
+                {!hasValue && null}
+                {length === 1 && (value[0].label)}
+                {length === 2 && ([value[0].label, 'and', value[1].label].join(' '))}
+                {length > 2 && ([`${value[0].label},`,value[1].label, 'and', abLength, 'more'].join(' '))} 
+                {children}
+            </components.ValueContainer>
+        )
+    }
+
+
+    const DropdownIndicator = (props: DropdownIndicatorProps<any>) => {
+        return (
+            <components.DropdownIndicator {...props}>
+                <ArrowDown />
+            </components.DropdownIndicator>
+        )
+    }
+
+    const Option = (props: OptionProps<any>) => {
+        return (
+            <components.Option {...props}>
+                <Fragment>
+                    <span className='box'>
+                        {props.isSelected ? <Check /> : <UnCheck />}
+                        <label>{props.label}</label>
+                    </span>
+                </Fragment>
+            </components.Option>
+        )
+    }
+    const handleChange = (e: any) => {
+        const { name, onChange } = props;
+        onChange(name, e);
+    }
+
+    return (
+        <Fragment>
+            <FormControl className='select' >
+                <InputLabel shrink htmlFor={`${props.id}-label`} style={{ color: 'var(--Darkgray)' }} aria-labelledby={props.label} aria-required={props.required}>
+                    <b>{props.label.toUpperCase()}{props.required && props.label && (<span className='super' >* </span>)}</b >
+                </InputLabel>
+                <Select
+                    id={props.id}
+                    name={props.name}
+                    placeholder={props.placeholder}
+                    className={props.error ? 'multi-select-container error' : 'multi-select-container'}
+                    classNamePrefix='multi-select'
+                    value={props.value}
+                    options={props.items}
+                    onChange={handleChange}
+                    onBlur={props.onBlur}
+                    components={{ ValueContainer, ClearIndicator: () => null, IndicatorSeparator: () => null, Option, DropdownIndicator}}
+                    isSearchable={false}
+                    hideSelectedOptions={false}
+                    closeMenuOnSelect={false}
+                    isMulti={true}
+                    //menuIsOpen
+                />
+                {props.helperText && (
+                    <FormHelperText
+                        className='select-helper'
+                        id={props.description}
+                        error={props.error}>
+                        {props.helperText}
+                    </FormHelperText>)}
+            </FormControl>
+        </Fragment>
+    );
+
+
+}
+
+MultiSelect.defaultProps = {
+    required: false,
+    id: "select-label",
+    error: false,
+    placeholder: 'Select',
+}
