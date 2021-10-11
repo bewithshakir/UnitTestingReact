a2b6ea5eed2a8631f94f64c891d47eceb677405c
Sai-G-SOM <87060290+Sai-G-SOM@users.noreply.github.com>
2021-10-11 21:29:14 +0530

    Update SideBarMenu.component.tsx

--- src/components/UIComponents/SideBarMenu/SideBarMenu.component.tsx
+++ src/components/UIComponents/SideBarMenu/SideBarMenu.component.tsx
@@ -8,18 +8,25 @@ import logoTwo from '../../../assets/images/Shell Taup logo2.svg';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { BriefCase64Icon, Invoice64Icon, ToggleList64Icon, Truck64Icon, DocumentFile64Icon, ToggleMail64Icon, ChartPie64Icon } from '../../../assets/icons';
-import { BrowserRouter, useHistory } from "react-router-dom";
+import {Link, Route, Switch, useHistory } from "react-router-dom";
+import HorizontalBar from '../NavigationBar/HorizontalBar';
+import { Fragment, Suspense } from 'react';
+import { routes } from '../../../routes';
+import Page from '../../../navigation/Page';
+import { CssBaseline } from '@mui/material';
+import { Loader } from '../Loader';
 
 const SideBarMenuoptions = [{
   index: 0,
   icon: <BriefCase64Icon />,
   text: 'Home',
-  route: 'home'
-}, {
+  route: '/'
+},
+{
   index: 1,
   icon: <Invoice64Icon />,
   text: 'Dashboard',
-  route: 'dashboard'
+  route: '/dashboard'
 }, {
   index: 2,
   icon: <ToggleList64Icon />,
@@ -40,40 +47,36 @@ const SideBarMenuoptions = [{
   icon: <ChartPie64Icon />,
   text: 'Dashboard',
   route: 'home'
-}, {
+},
+{
   index: 6,
   icon: <DocumentFile64Icon />,
   text: 'Dashboard',
-  route: 'home'
-}, {
-  index: 7,
-  icon: <DocumentFile64Icon />,
-  text: 'Dashboard',
-  route: '/demo'
+  route: 'demo'
 }];
 
 // type SideBarMenuOption = {
 //   index: number;
 //   icon?: React.ReactNode;
 //   text?: string;
-//   route?: string;
+//   route?: any;
 //   component?: React.ReactNode;
 //   to?: any;
 // }
 // interface sideBarMenuProps {
 //   options?: SideBarMenuOption[],
 //   onSelect?: (selectedValue: any) => void
+//   children?: any
 // }
 
-export default function SideBarDrawer() {
+export default function SideBarDrawer() { 
   const { themeType } = useTheme();
   const logoSrc = themeType === 'UK' ? logoOne : logoTwo;
   const [value, setValue] = React.useState(0);
-  const history = useHistory();
 
   const onSelectOptions = (value: number) => {
-    if(value == 7) {
-      history.push("/demo");
+    if (value == 6) {
+      // history.push("/demo")
     }
   };
 
@@ -82,35 +85,61 @@ export default function SideBarDrawer() {
     onSelectOptions(newValue);
   };
 
+
+  const history = useHistory();
+  function onClickBack() {
+    history.push("/");
+  }
+
   const drawerWidth = 64;
   return (
     <Box className={'sidebar-menu'}>
-      <Drawer className={'sidebar-drawer'}
-        sx={{
-          width: drawerWidth,
-        }}
-        variant="permanent"
-        anchor="left">
-        <div>
-          <img className="sidebarmenu_logo"
-            src={logoSrc}
-            alt="logo" />
-        </div>
-        <BrowserRouter>
-        <Tabs
-          orientation="vertical"
-          value={value}
-          onChange={handleChange}
-          aria-label="sidebar menu"
-          className={'sidebarmenu_tabs'} >
-          {SideBarMenuoptions && SideBarMenuoptions.map((item, index) => {
-            return (
-              <Tab className={'sidebarmenu_tab'} icon={item.icon} key={index} />
-            );
-          })}
-        </Tabs>
-        </BrowserRouter>
-      </Drawer>
+      <CssBaseline />
+      {routes.map((route,index) => {
+   
+        return (
+          <Fragment key={index}>
+            <HorizontalBar
+              onBack={onClickBack}
+            />
+            <Drawer className={'sidebar-drawer'}
+              sx={{
+                width: drawerWidth,
+              }}
+              variant="permanent"
+              anchor="left">
+              <div>
+                <img className="sidebarmenu_logo"
+                  src={logoSrc}
+                  alt="logo" />
+              </div>
+
+              <Tabs
+                orientation="vertical"
+                value={value}
+                onChange={handleChange}
+                aria-label="sidebar menu"
+                className={'sidebarmenu_tabs'} >
+                {SideBarMenuoptions && SideBarMenuoptions.map((item, index) => {
+                  return (
+                    <Tab className={'sidebarmenu_tab'} icon={item.icon} component={Link} to={item.route} key={index} />
+                  );
+                })}
+              </Tabs>
+
+            </Drawer>
+            <main className="content1">
+            <Suspense fallback={<Loader />}>
+                <Switch>
+                  <Route key={route.path} path={route.path} exact={route.exact}>
+                    <Page route={route} />
+                  </Route>
+                </Switch>
+              </Suspense>
+            </main>
+          </Fragment>
+        );
+      })}
     </Box>
   );
 }
