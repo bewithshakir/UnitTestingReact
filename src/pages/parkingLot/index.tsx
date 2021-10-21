a90ebbabdc43ed70035a2912d1e08fff2b82b8b4
Sai.S.Krishna <Sai.S.Krishna@shell.com>
2021-10-21 12:25:10 +0530

    Parking Lot API Integration

--- src/pages/parkingLot/index.tsx
+++ src/pages/parkingLot/index.tsx
@@ -1,19 +1,198 @@
-import { Button } from '@mui/material';
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-
-
-function ParkingLot(props: any): React.ReactElement {
-    console.warn(props);
-    const history = useHistory();
-    const onItemClick = () => {
-        history.push("/customer/parkingLots/addLot");
-    };
-    return (
-        <div>
-            <Button onClick={onItemClick} style={{margin:40}}>{"Add Lot"}</Button>
-        </div>
-    );
+import React, { SyntheticEvent } from "react";
+import { Button } from "../../components/UIComponents/Button/Button.component";
+import "./style.scss";
+import { useTranslation } from "react-i18next";
+import SortbyMenu from "../../components/UIComponents/Menu/SortbyMenu.component";
+import ActionsMenu from "../../components/UIComponents/Menu/ActionsMenu.component";
+import {
+  ExportIcon,
+  PlusIcon,
+  DeleteIcon,
+  ImportIcon,
+  FilterIcon,
+} from "../../assets/icons";
+import GridComponent from "../../components/UIComponents/DataGird/grid.component";
+import { useCustomers } from "./queries";
+import SearchInput from "../../components/UIComponents/SearchInput/SearchInput";
+import { Add } from "@mui/icons-material";
+import { useHistory } from "react-router-dom";
+import { sortByOptions } from "./config";
+import { RightInfoPanel } from "../../components/UIComponents/RightInfoPanel/RightInfoPanel.component";
+import { Box, FormControl, Grid } from "@mui/material";
+import { HorizontalBarVersionState, useStore } from "../../store";
+interface ContentProps {
+  rows?: [];
+  version: string
 }
+
+const headCells = [
+  { id: "deliveryLocationNm", label: "LOT NAME", type: 'text' },
+  { id: "streetAddress", label: "STREET ADDRESS", type: 'text' },
+  { id: "cityNm", label: "CITY", type: 'text' },
+  { id: "stateNm", label: "STATE", type: 'text' },
+  { id: "postalCd", label: "ZIP", type: 'text' },
+  { id: "state", label: "RACK UPDATE", type: 'text' },
+  { id: "zipCode", label: "WALLET STATUS", type: 'text' },
+  { id: "lots", label: "FUEL", type: 'text' },
+  { id: "lots", label: "VEHICLES", type: 'button' },
+];
+
+const ParkingLot: React.FC<ContentProps> = () => {
+  const history = useHistory();
+  const [info, setInfo] = React.useState({});
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+  const [sortOrder, setSortOrder] = React.useState<{ sortBy: string, order: string }>({ sortBy: "customerName", order: "asc" });
+  const [filterData, setFilterData] = React.useState<{ [key: string]: string[] }>({});
+  const [custFilterPanelVisible, setCustFilterPanelVisible] = React.useState(false);
+  const customerId = "fc2ffe5e-7ef8-46b8-95c2-cb82cf77ed90";
+
+  const { t } = useTranslation();
+  const { data, fetchNextPage, isLoading }: any = useCustomers(
+    searchTerm,
+    sortOrder,
+    filterData,
+    customerId
+  );
+  const setVersion = useStore((state: HorizontalBarVersionState) => state.setVersion);
+  setVersion("NavLinks");
+  const openDrawer = (row: SyntheticEvent) => {
+    setInfo(row);
+    setDrawerOpen(true);
+  };
+  const drawerClose = () => {
+    setDrawerOpen(false);
+  };
+
+  const onSortBySlected = (value: string) => {
+    let sortOrder;
+    switch (value) {
+      case "Z-A":
+        sortOrder = { sortBy: "customerName", order: "desc" };
+        break;
+      case "Newest to Oldest":
+        sortOrder = { sortBy: "date", order: "desc" };
+        break;
+      case "Oldest to New":
+        sortOrder = { sortBy: "date", order: "asc" };
+        break;
+      default:
+        sortOrder = { sortBy: "customerName", order: "asc" };
+        break;
+    }
+    setSortOrder(sortOrder);
+  };
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.currentTarget.value);
+  };
+  const list: any = [];
+  data?.pages?.map((item: any) => {
+    list.push(...item.data.lots);
+  });
+
+
+  const handleCustFilterPanelOpen = () => {
+    setDrawerOpen(false);
+    setCustFilterPanelVisible(!custFilterPanelVisible);
+  };
+
+  const onItemClick = () => {
+    history.push("/customer/parkingLots/addLot");
+  };
+
+  const handleCustFilterPanelClose = () => setCustFilterPanelVisible(false);
+
+  const getFilterParams = (filterObj: { [key: string]: string[] }) => setFilterData(filterObj);
+
+  return (
+    <Box display="flex">
+      <Grid container pl={2.25} pr={6.25} className="main-area">
+        <Grid container display="flex" flexGrow={1}>
+          <Grid item md={8} lg={9} display="flex" >
+            <Grid item pr={2.5}>
+              <Button
+                types="filter"
+                aria-label="dafault"
+                onClick={handleCustFilterPanelOpen}
+                startIcon={<FilterIcon />}
+              >
+                Filter
+              </Button>
+            </Grid>
+            <Grid item pr={2.5}>
+              <FormControl>
+                <SortbyMenu
+                  options={sortByOptions.map((sortByItem) => t(sortByItem))}
+                  onSelect={(value) => onSortBySlected(value)}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <SearchInput
+                name="searchTerm"
+                value={searchTerm}
+                onChange={onInputChange}
+              />
+            </Grid>
+          </Grid>
+          <Grid item md={4} lg={3} display="flex" justifyContent="flex-end">
+            <Grid item pr={2.5}>
+              <Button
+                types="primary"
+                aria-label="primary"
+                onClick={onItemClick}
+                startIcon={<Add />}
+              >
+                {t("buttons.add lot")}
+              </Button>
+            </Grid>
+            <Grid item>
+              <FormControl>
+                <ActionsMenu
+                  options={[
+                    {
+                      label: t("menus.actions.add vehicle"),
+                      icon: <PlusIcon />,
+                    },
+                    {
+                      label: t("menus.actions.import data"),
+                      icon: <ImportIcon />,
+                    },
+                    {
+                      label: t("menus.actions.export data"),
+                      icon: <ExportIcon />,
+                    },
+                    {
+                      label: t("menus.actions.delete"),
+                      icon: <DeleteIcon />,
+                    },
+                  ]}
+                  onSelect={(value) => {
+                    return value;
+                  }}
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container pt={2.5} display="flex" flexGrow={1}>
+
+          <GridComponent
+            rows={list}
+            header={headCells}
+            isLoading={isLoading}
+            enableRowSelection
+            enableRowAction
+            getPages={fetchNextPage}
+            openDrawer={openDrawer}
+          />
+
+          <RightInfoPanel panelType="customer-filter" open={custFilterPanelVisible} headingText={"Filters"} provideFilterParams={getFilterParams} onClose={handleCustFilterPanelClose} />
+          <RightInfoPanel panelType="info-view" open={drawerOpen} headingText={"Accurate Transportation"} info={info} onClose={drawerClose} />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
 
 export default ParkingLot;
