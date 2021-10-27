2cc6cb42d388ae328badcac2445972093a44c80e
abhijeetshell <abhijeet.gupta@shell.com>
2021-10-27 09:47:38 +0530

    Added config for lot header

--- src/pages/ParkingLot/AddLot/AddLotWrapper.tsx
+++ src/pages/ParkingLot/AddLot/AddLotWrapper.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import AddLotForm from './AddLotForm/AddLotForm.component';
 import './AddLotWrapper.style.scss';
+import { addLotHeaderConfig } from '../config';
 
 
 interface TabPanelProps {
@@ -38,7 +38,7 @@ function TabPanel(props: TabPanelProps) {
         >
             {value === index && (
                 <Box sx={{ p: 3 }}>
-                    <Typography>{children}</Typography>
+                    <div>{children}</div>
                 </Box>
             )}
         </div>
@@ -57,43 +57,30 @@ const AddLotWrapper: React.FC<MenuProps> = () => {
         <Box sx={{ width: '100%' }}>
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                 <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                    <Tab label={<React.Fragment>
-                        <div className="tab-div">
-                        <div className="verticle-middle"> Lot Details  </div> 
-                        </div>
-                    </React.Fragment>} {...a11yProps(0)} />
-                    <Tab label={<React.Fragment>
-                        <div className="tab-div">
-                        <div className="verticle-middle">Products </div><div className='verticle-middle number-box'>0</div></div>
-                    </React.Fragment>} {...a11yProps(1)} />
-                    <Tab label={<React.Fragment>
-                        <div className="tab-div">
-                        <div className="verticle-middle">Fee Details </div></div>
-                    </React.Fragment>} {...a11yProps(2)} />
-                    <Tab label={<React.Fragment>
-                        <div className="tab-div">
-                        <div className="verticle-middle"> Items </div> <div className='verticle-middle number-box'>0</div></div>
-                    </React.Fragment>} {...a11yProps(1)} />
-                    <Tab label={<React.Fragment>
-                        <div className="tab-div">
-                        <div className="verticle-middle"> Wallets </div> <div className='verticle-middle number-box'>0</div></div>
-                    </React.Fragment>} {...a11yProps(2)} />
+                    {addLotHeaderConfig.map((headerTab, index) =>
+                        <Tab key={headerTab.index} label={<React.Fragment>
+                            <div className="tab-div">
+                                <div className="verticle-middle"> {headerTab.label}  </div>
+                                {headerTab.containsValue && <div className='verticle-middle number-box'>{headerTab.value}</div>}
+                            </div>
+                        </React.Fragment>} {...a11yProps(index)} />
+                    )}
                 </Tabs>
             </Box>
             <TabPanel value={value} index={0}>
                 <AddLotForm />
             </TabPanel>
             <TabPanel value={value} index={1}>
-                Item Two
+
             </TabPanel>
             <TabPanel value={value} index={2}>
-                Item Three
+
             </TabPanel>
             <TabPanel value={value} index={3}>
-                Item Four
+
             </TabPanel>
             <TabPanel value={value} index={4}>
-                Item Five
+
             </TabPanel>
         </Box>
     );
--- src/pages/ParkingLot/config.ts
+++ src/pages/ParkingLot/config.ts
@@ -3,3 +3,34 @@ export const sortByOptions = [
     "parkingLot.sortBy.paymentInProgress",
     "parkingLot.sortBy.recentlyAddedLots"
   ];
+
+  export const addLotHeaderConfig = [
+    {
+        label: "Lot Details",
+        index:0,
+        containsValue: false,
+    },
+    {
+      label: "Products",
+      index:1,
+      containsValue: true,
+      value: 0
+    },
+    {
+        label: "Fee Details",
+        containsValue: false,
+        index:2,
+    },
+    {
+      label: "Items",
+      index:3,
+      containsValue: true,
+      value:0
+    },
+    {
+      label: "Wallets",
+      index:4,
+      containsValue: true,
+      value:0
+    }
+];
