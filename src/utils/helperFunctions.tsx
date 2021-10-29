2797a22caeee4af089e67b99a7c12e571b419c21
abhijeetshell <abhijeet.gupta@shell.com>
2021-10-30 00:21:03 +0530

    Improvised helper function

--- src/utils/helperFunctions.tsx
+++ src/utils/helperFunctions.tsx
@@ -1,8 +1,8 @@
 export const maskPhoneNumber = (phNumber:string) => {
-    if(phNumber){
+    if (typeof phNumber === 'string'){
         const maskedPh = phNumber.match(/(\d{3})(\d{3})(\d{4})/);
         return maskedPh ?"(" + maskedPh[1] + ") " + maskedPh[2] + "-" + maskedPh[3]:phNumber;
     }else{
-        return phNumber
+        return phNumber;
     }
-}
+};
