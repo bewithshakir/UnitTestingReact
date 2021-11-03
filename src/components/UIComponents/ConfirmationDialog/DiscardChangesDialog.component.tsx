856a8d2ce7f5b27248ddc438c95654d1c5535439
Sai.S.Krishna <Sai.S.Krishna@shell.com>
2021-11-03 15:50:30 +0530

    Dialog Background Backdrop Issue

--- src/components/UIComponents/ConfirmationDialog/DiscardChangesDialog.component.tsx
+++ src/components/UIComponents/ConfirmationDialog/DiscardChangesDialog.component.tsx
@@ -19,7 +19,7 @@ function DiscardChangesDialog (props: Props) {
             open={open}
             aria-labelledby="discard-changes-dialog-title"
             aria-describedby="discard-changes-dialog-description"
-            BackdropProps={{ style: { backgroundColor: "transparent"} }}
+            BackdropProps={{ style: { backgroundColor: "gray", opacity: 0.3} }}
         >
             <div className="discard-dialog-container">
                 <Typography color="var(--Darkgray)" variant="h2" component="h2" className="fw-bold" px={2.5} pt={2.5} >
