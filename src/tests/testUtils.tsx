c7399a73b2bcbd0596abdc23f3c07302dfa1a156
mohammad Hussain <93126930+mohammadhussain2@users.noreply.github.com>
2021-11-22 11:33:44 +0530

    Changed updated

---
+++ src/tests/testUtils.tsx
@@ -0,0 +1,45 @@
+import { shallow } from "enzyme";
+import { createMemoryHistory } from "history";
+import { I18nextProvider } from "react-i18next";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Router } from "react-router-dom";
+import { i18n } from "../i18n/i18n";
+
+/**
+ * Returns node(s) with the given data-test attribute.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
+ * @param {string} val - Value of data-test attribute to search. 
+ * @returns {ShallowWrapper}
+ */
+export const findByTestAttr = (wrapper: any, val: any)=> wrapper.find(`[data-test="${val}"]`);
+
+export const findByIdAttr = (wrapper: any, val: any)=> wrapper.find(`#${val}`);
+
+export const HOCSetup = (props: any, route: any)=> {
+    const queryClient = new QueryClient();
+    const history = createMemoryHistory();
+    if (route) {
+        history.push(route);
+    }
+    return (
+        <QueryClientProvider client={queryClient}>
+            <I18nextProvider i18n={i18n}>
+                <Router history={history}>
+                    {props.children}
+                </Router>
+            </I18nextProvider>
+        </QueryClientProvider>
+    );
+};
+
+export const renderWithRouter = (renderComponent, route) => {
+    const history = createMemoryHistory();
+  
+    if(route){
+      history.push(route)
+    }
+  
+    return {
+      ...shallow(<Router history={history}>{renderComponent()}</Router>), history
+    }
+};
