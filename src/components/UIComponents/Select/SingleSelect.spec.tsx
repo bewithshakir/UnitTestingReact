20d682c87e43c262cc901a83c34169d4bb500295
abhijeetshell <abhijeet.gupta@shell.com>
2021-11-23 11:26:30 +0530

    Added code for snap shot testing for icon

--- src/components/UIComponents/Select/SingleSelect.spec.tsx
+++ src/components/UIComponents/Select/SingleSelect.spec.tsx
@@ -1,8 +1,9 @@
 import Select from './SingleSelect';
+import { YellowFuelIcon } from '../../../assets/icons';
 import { shallow } from 'enzyme';
 
 describe('renders the Default Select component', () => {
-    const component = shallow(<Select label='Input' id='input' items={[{label:'Label', value: 'Value'}]} onChange={() => jest.fn()}/>);
+    const component = shallow(<Select label='Input' id='input' items={[{label:'Label', value: 'Value', icon: <YellowFuelIcon/>}]} onChange={() => jest.fn()}/>);
 
     it('snapshot test for Default Select component', () => {
         expect(component).toMatchSnapshot();
