191037277083f07c0a3486482b2f882a68271867
abhijeetshell <abhijeet.gupta@shell.com>
2021-11-24 07:13:35 +0530

    Test cases written with issues fixes on single and multiselect component

--- src/components/UIComponents/Select/SingleSelect.spec.tsx
+++ src/components/UIComponents/Select/SingleSelect.spec.tsx
@@ -1,9 +1,9 @@
 import Select from './SingleSelect';
 import { YellowFuelIcon } from '../../../assets/icons';
-import { shallow } from 'enzyme';
+import { shallow , mount} from 'enzyme';
 
 describe('renders the Default Select component', () => {
-    const component = shallow(<Select label='Input' id='input' items={[{label:'Label', value: 'Value', icon: <YellowFuelIcon/>}]} onChange={() => jest.fn()}/>);
+    const component = shallow(<Select label='Input' items={[{label:'Label', value: 'Value', icon: <YellowFuelIcon/>}]} onChange={() => jest.fn()}/>);
 
     it('snapshot test for Default Select component', () => {
         expect(component).toMatchSnapshot();
@@ -11,3 +11,83 @@ describe('renders the Default Select component', () => {
 
 });
 
+describe('renders the SingleSelect component with values', () => {
+    const component = shallow(<Select label='Input'  value={{ label: 'Label1', value: 'Value1' , icon: <YellowFuelIcon/>}} items={[{ label: 'Label1', value: 'Value1' , icon: <YellowFuelIcon/>}, { label: 'Label2', value: 'Value2', icon: <YellowFuelIcon/> }, { label: 'Label3', value: 'Value3', icon: <YellowFuelIcon/> }]} onChange={() => jest.fn()} />);
+
+    it('snapshot test for the SingleSelect component ', () => {
+        expect(component).toMatchSnapshot();
+    });
+});
+
+
+describe('renders the SingleSelect component with error ', () => {
+    const component = shallow(<Select label='Input' error={true} value={{ label: 'Label1', value: 'Value1' , icon: <YellowFuelIcon/>}} items={[{ label: 'Label1', value: 'Value1' , icon: <YellowFuelIcon/>}, { label: 'Label2', value: 'Value2', icon: <YellowFuelIcon/> }, { label: 'Label3', value: 'Value3', icon: <YellowFuelIcon/> }]} onChange={() => jest.fn()} />);
+
+    it('snapshot test for the SingleSelect component ', () => {
+        expect(component).toMatchSnapshot();
+    });
+});
+
+describe('renders the SingleSelect component with required prop', () => {
+    const component = shallow(<Select label='Input' required  value={{ label: 'Label1', value: 'Value1' , icon: <YellowFuelIcon/>}} items={[{ label: 'Label1', value: 'Value1' , icon: <YellowFuelIcon/>}, { label: 'Label2', value: 'Value2', icon: <YellowFuelIcon/> }, { label: 'Label3', value: 'Value3', icon: <YellowFuelIcon/> }]} onChange={() => jest.fn()} />);
+
+    it('snapshot test for the SingleSelect component ', () => {
+        expect(component).toMatchSnapshot();
+    });
+});
+
+describe('renders the SingleSelect component with helper text', () => {
+    const component = shallow(<Select label='Input' required helperText={'sample error'}  value={{ label: 'Label1', value: 'Value1' , icon: <YellowFuelIcon/>}} items={[{ label: 'Label1', value: 'Value1' , icon: <YellowFuelIcon/>}, { label: 'Label2', value: 'Value2', icon: <YellowFuelIcon/> }, { label: 'Label3', value: 'Value3', icon: <YellowFuelIcon/> }]} onChange={() => jest.fn()} />);
+
+    it('snapshot test for the SingleSelect component ', () => {
+        expect(component).toMatchSnapshot();
+    });
+});
+
+describe('renders the SingleSelect component on change', () => {
+    it('Should change value when onChange was called', () => {
+        const props = {
+            label: 'Select values',
+            onChange: jest.fn(),
+            id:'single-select-on-change-test',
+            items: [{ label: 'Label1', value: 'Value1' , icon: <YellowFuelIcon/>}, { label: 'Label2', value: 'Value2', icon: <YellowFuelIcon/> }, { label: 'Label3', value: 'Value3', icon: <YellowFuelIcon/> }]
+        }
+        const wrapper = mount(<Select {...props} />);
+        const event = { label: 'Label1', value: 'Value1' , icon: <YellowFuelIcon/>};
+        wrapper.find('Select').simulate('change', event)
+        expect(wrapper).toMatchSnapshot();
+    });
+});
+
+
+describe('renders the SingleSelect component with correct no of options', () => {
+    it('Should render correct list of options', () => {
+        const options = [{ label: 'Label1', value: 'Value1' , icon: <YellowFuelIcon/>}, { label: 'Label2', value: 'Value2', icon: <YellowFuelIcon/> }];
+        const props = {
+            label: 'Select values',
+            onChange: jest.fn(),
+            id:'single-select',
+            items: options
+        }
+        const wrapper = mount(<Select {...props} />);
+        wrapper.find('div.react-select__dropdown-indicator').simulate('mouseDown', {button: 0 });
+        expect(wrapper.find('div.react-select__option').length).toEqual(options.length);
+    });
+});
+
+describe('renders the SingleSelect component with option selected', () => {
+    it('Should render correct text in value', () => {
+        const randomNumber = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+        const options = [{ label: 'Label1', value: 'Value1' , icon: <YellowFuelIcon/>}, { label: 'Label2', value: 'Value2', icon: <YellowFuelIcon/> }, { label: 'Label3', value: 'Value3', icon: <YellowFuelIcon/> }];
+        const props = {
+            label: 'Select values',
+            onChange: jest.fn(),
+            id:'single-select-options-check',
+            items: options
+        }
+        const wrapper = mount(<Select {...props} />);
+        wrapper.find('div.react-select__dropdown-indicator').simulate('mouseDown', {button: 0 });
+        wrapper.find('div.react-select__option').at(1).simulate('click', null);
+        expect(wrapper.find('span.label-text').text()).toEqual(options[1].label);
+    });
+});
