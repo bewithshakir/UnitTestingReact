26322ff770bf2757c3037aff3f73121770069bb0
Sai.S.Krishna <Sai.S.Krishna@shell.com>
2021-12-07 21:00:46 +0530

    Code revert

--- src/pages/AddFuelTax/validation.ts
+++ src/pages/AddFuelTax/validation.ts
@@ -4,7 +4,7 @@ export const AddFuelTaxValidationSchema = Yup.object().shape({
     addressLine1: Yup.string().required('Required'),
     city: Yup.string().required('Required'),
     state: Yup.string().required('Required'),
-    fuelType: Yup.object().shape({ label: Yup.string(), value: Yup.string() }),
+    fuelType: Yup.object().shape({ label: Yup.string().required('Required'),value: Yup.string().required('Required')}).required('Required'),
     federalRate: Yup.string().matches(/^[0-9]+([.][0-9]+)?$/, 'Invalid Rate.').required('Required'),
     localRate: Yup.string().matches(/^[0-9]+([.][0-9]+)?$/, 'Invalid Rate.').required('Required'),
     salesFuelRate: Yup.string().matches(/^[0-9]+([.][0-9]+)?$/, 'Invalid Rate.').required('Required'),
