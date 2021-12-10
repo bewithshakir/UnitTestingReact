03e212a0bd7e332d23ec3e31598e5b579eddd9de
Nupur.Hattekar <Nupur.Hattekar@shell.com>
2021-12-10 16:21:31 +0530

    UTC for Edit Parking Lot

---
+++ src/pages/ParkingLot/AddLot/AddLotForm/AddLotForm.spec.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+import { mount } from "enzyme";
+import AddLotForm from './AddLotForm.component';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom") as any,
+    useLocation: () => ({
+        pathname: "localhost:3000/customer/123456/parkingLots/viewLot/89898"
+    })
+}));
+
+beforeAll(() => {
+    jest.useFakeTimers('modern');
+    jest.setSystemTime(new Date(1638338756741));
+});
+
+const queryClient = new QueryClient();
+
+describe('Rendering of View Lot Component', () => {
+    it('Edit/View Lot component Snapshot testing when', () => {
+        console.log('Normal: 34  ', new Date().getTime());
+        const component = mount(<QueryClientProvider client={queryClient}>
+            <AddLotForm />
+        </QueryClientProvider>
+        );
+
+        expect(component).toMatchSnapshot();
+    });
+});
+
+afterAll(() => {
+    jest.useRealTimers();
+});
