41c5a60b79f2532bb16e9ea4bef4af81a11bd290
Rasheed <rasheed.shaik@shell.com>
2021-12-13 16:27:30 +0530

    parking lot api integration

--- src/pages/ParkingLot/ParkingLot.spec.tsx
+++ src/pages/ParkingLot/ParkingLot.spec.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import GridComponent from "../../components/UIComponents/DataGird/grid.component";
 import ParkingLotContent from './index';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import SortbyMenu from '../../components/UIComponents/Menu/SortbyMenu.component';
@@ -27,11 +27,10 @@ beforeAll(() => {
     jest.setSystemTime(new Date(1638338756741));
 });
 
-describe('Rendering of Search in Parking Lot Content Component', () => {
+describe('Rendering of Parking Lot Content Component', () => {
     const component = mount(<QueryClientProvider client={queryClient}><ParkingLotContent version='1' /></QueryClientProvider>);
     it('Parking Lot Content component Snapshot testing when', () => {
         expect(component).toMatchSnapshot();
-        
     });   
 });
 
@@ -60,7 +59,40 @@ describe('Given Sortby Menu on ParkingLot Landing Page', () => {
 
 });
 
-
-
+describe('Edit in grid Component Parking Lot view', () => {
+    const mockData = {
+        data:{
+        lots: {
+            cityNm: "Culpeper",
+            customerId: "83f01d4e-6b26-432f-97e5-92413c1d73df",
+            deliveryLocationId: "002a061f-8643-4e97-96b1-9e65f2bff0f6",
+            deliveryLocationNm: "L1",
+            fuelStatus: ["Regular", "Premium", "Diesel"],
+            postalCd: "22701",
+            primaryContactName: "abc s1",
+            rackUpdate: "27-02-2021 7:09 PM",
+            stateNm: "VA",
+            streetAddress: "ABC Auto Sales Inc,  Rixeyville Rd,,   Rixeyville Rd, rixvillie",
+            vehicles: 20,
+            walletStatus: "Y"
+        },
+        pagination: { totalCount: 1, limit: 15, offset: 0 },
+        error: null
+    }
+    };
+    jest.mock("./queries", () => {
+        return {
+            useGetParkingLotDetails: jest.fn().mockReturnValue({ mockData })
+        };
+    });
+    const component = mount(<QueryClientProvider client={queryClient}><ParkingLotContent version='1' /></QueryClientProvider>);
+    it('Parking Lot Content component Snapshot testing when', () => {
+        expect(component.find(GridComponent)).toBeDefined();
+        expect(component.find(GridComponent)).toHaveLength(1);
+        expect(component.find('.div.btn-grid-action')).toBeDefined();
+        expect(component.find('.datagrid-actions-menu')).toBeDefined();
+  
+    });
+});
 
 
