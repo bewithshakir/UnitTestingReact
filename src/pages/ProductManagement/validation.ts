646f55600eea7824e8a41083e510c1f5c8bdccf8
abhijeetshell <abhijeet.gupta@shell.com>
2021-12-13 21:45:03 +0530

    top row hide/show issue fixed on landing on product page

--- src/pages/ProductManagement/validation.ts
+++ src/pages/ProductManagement/validation.ts
@@ -1,5 +1,4 @@
 import * as Yup from 'yup';
-<<<<<<< HEAD
 const selectOption = Yup.object().shape({ label: Yup.string().required('Required'), value: Yup.string().required('Required') }).required('Required');
 const selectOptional = Yup.object().shape({ label: Yup.string(), value: Yup.string() }).required('Required');
 const priceSchema = Yup.string().matches(/^\d+(\.\d+)?$/, 'Invalid Price.');
@@ -7,15 +6,6 @@ export const AddProductValidationSchema = Yup.object().shape(
     {
         productType: selectOption,
         masterProductName: selectOption,
-=======
-const selectOption = Yup.object({ label: Yup.string(), value: Yup.string() }).required('Required');
-const priceSchema = Yup.string().matches(/^\d+(\.\d+)?$/, 'Invailid Price.');
-export const AddProductValidationSchema = Yup.object(
-    {
-        paymentType: selectOption.required('Required'),
-        productType: selectOption.required('Required'),
-        masterProductName: selectOption.required('Required'),
->>>>>>> d91bdc4 (enh: solve decimal/validation issue)
         pricingModel: selectOption,
         productNm: Yup.string().required('Required'),
         manualPriceAmt: priceSchema.required('Required'),
@@ -23,10 +13,6 @@ export const AddProductValidationSchema = Yup.object(
         discountPriceAmt: priceSchema.label('this').when(['manualPriceAmt', 'addedPriceAmt'], (manualPriceAmt: string, addedPriceAmt: any) => {
             return Yup.number().label('this').max(Number(manualPriceAmt) + Number(addedPriceAmt));
         }),
-<<<<<<< HEAD
         timeSlot: selectOptional
-=======
-        timeSlot: selectOption
->>>>>>> d91bdc4 (enh: solve decimal/validation issue)
     }
 );
