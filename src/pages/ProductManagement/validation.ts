573042aa892ca82d514b865887cad8277b7cd63d
shambhu-svyc <shambhu.verma@shell.com>
2021-12-15 15:07:43 +0530

    fix: 1096327 UI issue

--- src/pages/ProductManagement/validation.ts
+++ src/pages/ProductManagement/validation.ts
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-const selectOption = Yup.object({ label: Yup.string(), value: Yup.string() }).required('Required');
+const selectOption = Yup.object({ label: Yup.string(), value: Yup.string()}).required('Required');
 const priceSchema = Yup.string().matches(/^\d+(\.\d+)?$/, 'Invailid Price.');
 export const AddProductValidationSchema = Yup.object(
     {
