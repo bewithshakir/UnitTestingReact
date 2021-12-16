14bb5c7c954e395ed8b1b24eb293c93417c56ec6
Nikhil Patel <nikhil.patel2@shell.com>
2021-12-16 16:05:39 +0530

    964032 : UTC

---
+++ src/pages/OPISCity/opisCities.spec.tsx
@@ -0,0 +1,40 @@
+import { mount } from 'enzyme';
+import OPISCityLendingPage from "./index";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { OPISCity } from './config';
+
+const queryClient = new QueryClient();
+describe('Given OPIS Cities Landing Page', () => {
+    const Page = mount(<QueryClientProvider client={queryClient}>
+        <OPISCityLendingPage />
+    </QueryClientProvider>
+    );
+    test('Page Rendered Properly', () => {
+        expect(Page).toMatchSnapshot();
+    });
+    test('Filter Menu Rendered With Options', () => {
+        const Filter = Page.find('[types="filter"]');
+        Filter.simulate('click');
+        const DynamicFilterContent = Page.find('DynamicFilterContent');
+        const DynamicFilterProps = DynamicFilterContent.props();
+        expect(DynamicFilterProps['fields'].length).toBe(OPISCity.LandingPage.FilterByFields.length);
+        DynamicFilterProps['fields'].forEach((node, index) => {
+            expect(node.name).toBe(OPISCity.LandingPage.FilterByFields[index].name);
+        });
+    });
+    test('Sortby Menu Rendered With Options', () => {
+        const SortbyMenu = Page.find('SortbyMenu');
+        SortbyMenu.simulate('click');
+        expect(SortbyMenu.find('.sortby-popper').exists()).toBe(true);
+    });
+    test('Bruger Menu Rendered With Options', () => {
+        const BurgerMenu = Page.find('ActionsMenu');
+        BurgerMenu.simulate('click');
+        expect(BurgerMenu.find('.actions-popper').exists()).toBe(true);
+    });
+    test('Kabab Menu Rendered With Options', () => {
+        const BurgerMenu = Page.find('ActionsMenu');
+        BurgerMenu.simulate('click');
+        expect(BurgerMenu.find('.actions-popper').exists()).toBe(true);
+    });
+});
