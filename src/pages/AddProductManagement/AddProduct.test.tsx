7614968fa3183eacf0eb4a717ec99829a28b104e
Mohammad.Hussain2 <mohammad.hussain2@MohammadHussain2s-MacBook-Pro.local>
2021-12-17 11:01:39 +0530

    Test file added

---
+++ src/pages/AddProductManagement/AddProduct.test.tsx
@@ -0,0 +1,12 @@
+import { screen } from '@testing-library/dom';
+import { renderWithClient } from '../../tests/utils';
+
+import AddProduct from './AddProduct';
+
+describe('AddProduct component', ()=> {
+    it('renders AddProduct', async()=> {
+        const result = renderWithClient(<AddProduct/>);
+        expect(result).toBeDefined();
+    });
+    
+});
