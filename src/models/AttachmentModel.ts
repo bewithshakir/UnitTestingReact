119b39aa9462f8b405af9a2fe547744dd54d212d
Rasheed <rasheed.shaik@shell.com>
2022-01-17 15:01:48 +0530

    Added to translations

--- src/models/AttachmentModel.ts
+++ src/models/AttachmentModel.ts
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next";
 import { headerObj } from './../components/UIComponents/DataGird/grid.component';
 import { formatDateAsMMDDYYYY } from '../utils/DateHelpers';
 
@@ -13,12 +14,13 @@ export default class AttachmentsModel {
     }
 
     fieldsToDisplay(): headerObj[] {
+        const { t } = useTranslation();
         return [
-            { field: "documentName", label: "DOCUMENT NAME", type: 'text' },
-            { field: "documentFormat", label: "FORMAT", type: 'text' },
-            { field: "dateAdded", label: "DATE ADDED", type: 'text' },
-            { field: "uploadedBy", label: "UPLOADED BY", type: 'text' },
-            { field: "uploadedIn", label: "UPLOADED IN", type: 'text' },
+            { field: "documentName", label:t('Attachments.documentName'), type: 'text' },
+            { field: "documentFormat", label: t('Attachments.format') , type: 'text' },
+            { field: "dateAdded", label: t('Attachments.dateAdded'), type: 'text' },
+            { field: "uploadedBy", label: t('Attachments.uploadedBy'), type: 'text' },
+            { field: "uploadedIn", label: t('Attachments.uploadedIn'), type: 'text' },
         ];
     }
 }
