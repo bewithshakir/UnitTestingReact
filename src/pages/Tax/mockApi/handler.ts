5750611a2af10110ea535081459c717c992799fb
Rasheed <rasheed.shaik@shell.com>
2022-01-31 16:56:18 +0530

    Added Test Case

--- src/pages/Tax/mockApi/handler.ts
+++ src/pages/Tax/mockApi/handler.ts
@@ -34,3 +34,58 @@ export const getFuelTaxList = () => {
     });
 };
 
+export const getFuelTaxProductsList = () => {
+    return rest.get('*/api/tax-service/fueltax/list/products?countryCode=us&limit=15&offset=0&taxJurisdictionId=99zz', (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({
+                data: {
+                    pagination: {
+                        "totalCount": 2,
+                        "limit": 15,
+                        "offset": 0
+                    },
+                    products: [
+                        {
+                            "currencyCd": "USD",
+                            "productCd": "96c0b449-a2db-494e-947c-7be433c8ab45",
+                            "productGroupCd": "0b938129-4589-4d97-9932-c8d522c59eab",
+                            "startDate": "11/06/2021",
+                            "endDate": "01/31/2023",
+                            "productNm": "Premium",
+                            "miscInspFuelTax": 3,
+                            "ppdSalesTax": 5.101,
+                            "cityFuelTax": 6.034,
+                            "stateFuelTax": 21.345,
+                            "salesFuelRate": 5.245,
+                            "countyFuelTax": 1.56,
+                            "fedFuelTax": 11.499,
+                            "miscLoadFuelTax": 3.106,
+                            "revenueFuelRate": 23.44,
+                            "miscLocalFuelTax": 4
+                        },
+                        {
+                            "currencyCd": "USD",
+                            "productCd": "7215bc37-c9a5-468c-92c0-5d825e52daba",
+                            "productGroupCd": "0b938129-4589-4d97-9932-c8d522c59eab",
+                            "startDate": "11/06/2021",
+                            "endDate": "12/31/2023",
+                            "productNm": "Diesel",
+                            "ppdSalesTax": 5.101,
+                            "miscLocalFuelTax": 4,
+                            "fedFuelTax": 13.499,
+                            "miscLoadFuelTax": 3.106,
+                            "stateFuelTax": 20,
+                            "countyFuelTax": 1.56,
+                            "cityFuelTax": 1.034,
+                            "revenueFuelRate": 23.44,
+                            "salesFuelRate": 1.2,
+                            "miscInspFuelTax": 3
+                        }
+                    ]
+                },
+                error: null,
+            })
+        );
+    });
+};
--- src/pages/Tax/queries.spec.tsx
+++ src/pages/Tax/queries.spec.tsx
@@ -2,7 +2,7 @@ import { act, renderHook } from "@testing-library/react-hooks";
 import { rest } from "msw";
 import { serverMsw } from "../../setupTests";
 import { createWrapper } from "../../tests/utils";
-import { useFuelTaxList } from './queries';
+import { useFuelTaxList, getProducts } from './queries';
 
 
 describe('GetFuelTaxList', () => {
@@ -24,6 +24,36 @@ describe('GetFuelTaxList', () => {
             rest.get('*', (req, res, ctx) => res(ctx.status(500)))
         );
         const { result, waitFor } = renderHook(() => useFuelTaxList('', { sortBy: "", order: "" }, {}), {
+            wrapper: createWrapper()
+        });
+        act(() => {
+            result.current.fetchNextPage();
+        });
+
+        await waitFor(() => result.current.isError);
+        expect(result.current.error).toBeDefined();
+    });
+});
+
+describe('GetProductsByTaxId', () => {
+    it('Success returns data', async () => {
+        const { result, waitFor } = renderHook(() => getProducts('99zz'), {
+            wrapper: createWrapper()
+        });
+        act(() => {
+            result.current.fetchNextPage();
+        });
+        await waitFor(() => {
+            return result.current.isSuccess;
+        });
+        expect(result.current.status).toBe('success');
+    });
+
+    it('fail to get data', async () => {
+        serverMsw.use(
+            rest.get('*', (req, res, ctx) => res(ctx.status(500)))
+        );
+        const { result, waitFor } = renderHook(() => getProducts('99zz'), {
             wrapper: createWrapper()
         });
         act(() => {
