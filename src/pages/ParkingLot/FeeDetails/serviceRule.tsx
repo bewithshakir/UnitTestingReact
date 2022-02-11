a944412f4d18c3cc387feae45412250f5d998c13
Rasheed <rasheed.shaik@shell.com>
2022-02-11 14:24:34 +0530

    fixes CR's and validations

--- src/pages/ParkingLot/FeeDetails/serviceRule.tsx
+++ src/pages/ParkingLot/FeeDetails/serviceRule.tsx
@@ -16,6 +16,7 @@ type props = {
 }
 
 export default function ServiceRule({index, isDisabled, formik, lotId}: props) {
+    console.warn('======>', formik);
 
     const { t } = useTranslation();
 
@@ -165,7 +166,7 @@ export default function ServiceRule({index, isDisabled, formik, lotId}: props) {
                                 ? true : false
                         }
                         onBlur={() => { formik.setFieldTouched(`serviceFeeRules[${index}].productName`); formik.validateField(`serviceFeeRules[${index}].productName`); }}
-                        isDisabled={(formik?.values?.serviceFeeRules?.[index]?.productType?.value?.toLowerCase() === 'all') ? true : isDisabled}
+                        isDisabled={(formik?.values?.serviceFeeRules?.[index]?.productType?.value?.toLowerCase() === 'all') || (formik?.values?.serviceFeeRules?.[index]?.masterProductType?.value?.toLowerCase() === 'all') ? true : isDisabled}
                         required={(formik?.values?.serviceFeeRules?.[index]?.productType?.value?.toLowerCase() === 'all') ? false : true}
                     />
                 </Grid>
@@ -234,7 +235,7 @@ export default function ServiceRule({index, isDisabled, formik, lotId}: props) {
                     </Grid>
                 </Grid>
             }
-            <Grid container item md={12} mt={1} mb={1}>
+            {!(formik.values.serviceFeeRules[index].considerAsset) && <Grid container item md={12} mt={1} mb={1}>
                 <Grid item xs={12} md={6}>
                     <Select
                         id={`serviceFeeRules[${index}].vehicleType`}
@@ -261,6 +262,7 @@ export default function ServiceRule({index, isDisabled, formik, lotId}: props) {
                     />
                 </Grid>
             </Grid>
+            }
         </Fragment>
     );
 }
