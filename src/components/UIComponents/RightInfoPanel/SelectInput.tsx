ccf9cf4fd9bc727bc6b3d0cc98bb6a5b23375010
Nupur.Hattekar <Nupur.Hattekar@shell.com>
2022-03-01 15:27:41 +0530

    removed unwanted code

--- src/components/UIComponents/RightInfoPanel/SelectInput.tsx
+++ src/components/UIComponents/RightInfoPanel/SelectInput.tsx
@@ -1,9 +1,9 @@
 import { useTranslation } from "react-i18next";
-import { useGetCustomerNameData, useGetFilterData } from "../../../infrastructure/filterQuery";
+import { useGetFilterData } from "../../../infrastructure/filterQuery";
 import Select from "../Select/MultiSelect";
 import SingleSelect from "../Select/SingleSelect";
 import { filterURLKey } from '../../../infrastructure/filterQuery';
-
+import { useCustomers } from "../../../pages/CustomerManagement/queries";
 interface ISelectInput {
     field: {
         name: string;
@@ -20,15 +20,18 @@ interface ISelectInput {
 }
 export const SelectInput: React.FC<ISelectInput> = ({ field, handleSelect, formik }) => {
     const filterResponse = useGetFilterData(field.optionUrlKey);
-    const customerNameData = useGetCustomerNameData();
+    const customerNameData: any = useCustomers(
+        "",
+        { sortBy: "", order: "" },
+        {}
+    );
     const { t } = useTranslation();
-
     const touched = (formik.touched as any)[field.name];
     const error = (formik.errors as any)[field.name];
     const value = (formik.values as any)[field.name];
     const items = (field.optionAPIResponseKey == "customernames" && field.optionUrlKey == "parkingLotManagementFilter") ?
-     (customerNameData?.data?.data?.customers) ? 
-        customerNameData?.data?.data?.customers.map((obj: any) => ({ label: obj.customerName, value: obj.customerId})) : []
+    (customerNameData?.data?.pages[0]?.data?.customers) ?
+        customerNameData?.data?.pages[0]?.data?.customers.map((obj: any) => ({ label: obj.customerName, value: obj.customerId})) : []
         : filterResponse.status === 'success' && filterResponse.data?.data ?
         filterResponse.data.data[field.optionAPIResponseKey]?.map((s: any) => 
         (s.productNm ? {label: s.productNm, value: s.productCd} : { label: s, value: s })) 
