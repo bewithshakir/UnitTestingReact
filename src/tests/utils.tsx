014e346deaeeefc5bb05d6b5ed8cdae6275e519a
abhijeetshell <abhijeet.gupta@shell.com>
2022-03-01 16:12:39 +0530

    Added utc for prouct list

--- src/tests/utils.tsx
+++ src/tests/utils.tsx
@@ -7,7 +7,7 @@ import { getAllParkingLotsHandler } from '../pages/ParkingLotsManagement/mockApi
 import { getAttachmentListHandler } from '../pages/Attachments/mockApi/AttachmentLandingPageHandler';
 import { addDspHandler, getDspDataHandler, editDspHandler } from "../pages/DspLanding/AddDSP/mockApi/handlerAddDsp";
 import { getFormattedAddressHandler, getGoogleAddressHandler } from "../components/UIComponents/GoogleAddressComponent/mockApi/googleAddressAutoHandler";
-import { getProductByLotHandler, editProductHandler, getTaxExemptionsListHandler, getServedCitiesHandler, getSupplierPricesHandler, getSupplierBrandProductsHandler, getOpisRackProductDetailsHandler } from '../pages/ProductManagement/mockApi/handler';
+import { getProductByLotHandler, editProductHandler, getTaxExemptionsListHandler, getServedCitiesHandler, getSupplierPricesHandler, getSupplierBrandProductsHandler, getOpisRackProductDetailsHandler , getProductsHandler} from '../pages/ProductManagement/mockApi/handler';
 import { getFuelTaxList, getFuelTaxProductsList } from '../pages/Tax/mockApi/handler';
 import { getDelFeeSchduleHandler, getProductTypeHandler, getVehicleTypeHandler, getAssetTypeHandler, getLotMasterProductNamesHandler, getLotProductNamesHandler, addFeeDetailsHandler } from '../pages/ParkingLot/FeeDetails/mockAPi/handler';
 import { addTruckParkingLotHandler, editTruckParkingLotHandler } from '../pages/Truck/TruckParkingLot/AddLot/mockApi/handlerAddTruckParkingLot';
@@ -58,7 +58,8 @@ export const handlers = [
     addTruckFuelHandler(),
     addEditTruckHandler(),
     useGetEditTruckDetails(),
-    getOpisRackProductDetailsHandler()
+    getOpisRackProductDetailsHandler(),
+    getProductsHandler()
 ];
 
 const createTestQueryClient = () => new QueryClient({
