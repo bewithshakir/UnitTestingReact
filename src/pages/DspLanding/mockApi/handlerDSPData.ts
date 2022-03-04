fd4663a6528219085cf4469c0576bdfb51c39005
Nikhil Patel <nikhil.patel2@shell.com>
2022-03-04 13:02:16 +0530

    Sonar code duplicate issue fixes

--- src/pages/DspLanding/mockApi/handlerDSPData.ts
+++ src/pages/DspLanding/mockApi/handlerDSPData.ts
@@ -4,80 +4,38 @@ export const getDspDataHandler = () => {
     return rest.get('*/api/customer-service/customers/*/dsps*', (req, res, ctx) => {
         const search = req.url.searchParams.get('search');
         const limit = req.url.searchParams.get('limit');
-        return (search?.trim()) ?
-            search === "Test DSP" ?
-                res(
-                    ctx.status(200),
-                    ctx.json({
+        return res(
+            ctx.status(200),
+            ctx.json(
+                limit ?
+                    {
                         data: {
                             pagination: {
-                                "totalCount": 1,
+                                "totalCount": search?.trim() && search !== "Test DSP" ? 0 : 1,
                                 "limit": 15,
                                 "offset": 0
                             },
-                            dsps: [
-                                {
-                                    address: "12343 Bonfire Dr,  Bonfire Dr,   Bonfire Dr",
-                                    city: "Reisterstown",
-                                    contactName: "Test Contact",
-                                    customerId: "test-customer-id",
-                                    email: "d.s.h.j.s@h.dc.com",
-                                    id: "3a14f46b-0c85-441a-a532-4e068b3b6f3e",
-                                    lots: [],
-                                    name: "Test DSP",
-                                    postalCode: "21136",
-                                    state: "Test State",
-                                    totalLotAssigned: 0,
-                                }
-                            ]
+                            dsps:
+                                search !== "Test DSP" ? []
+                                    : [
+                                        {
+                                            address: "12343 Bonfire Dr,  Bonfire Dr,   Bonfire Dr",
+                                            city: "Reisterstown",
+                                            contactName: "Test Contact",
+                                            customerId: "test-customer-id",
+                                            email: "d.s.h.j.s@h.dc.com",
+                                            id: "3a14f46b-0c85-441a-a532-4e068b3b6f3e",
+                                            lots: [],
+                                            name: "Test DSP",
+                                            postalCode: "21136",
+                                            state: "Test State",
+                                            totalLotAssigned: 0,
+                                        }
+                                    ]
                         }
-                    })
-                ) :
-                res(
-                    ctx.status(200),
-                    ctx.json({
-                        data: {
-                            pagination: {
-                                "totalCount": 0,
-                                "limit": 15,
-                                "offset": 0
-                            },
-                            dsps: []
-                        }
-                    })
-                )
-            :
-            limit ?
-                res(
-                    ctx.status(200),
-                    ctx.json({
-                        data: {
-                            pagination: {
-                                "totalCount": 1,
-                                "limit": 15,
-                                "offset": 0
-                            },
-                            dsps: [
-                                {
-                                    address: "12343 Bonfire Dr,  Bonfire Dr,   Bonfire Dr",
-                                    city: "Reisterstown",
-                                    contactName: "Test Contact",
-                                    customerId: "test-customer-id",
-                                    email: "d.s.h.j.s@h.dc.com",
-                                    id: "3a14f46b-0c85-441a-a532-4e068b3b6f3e",
-                                    lots: [],
-                                    name: "Test DSP",
-                                    postalCode: "21136",
-                                    state: "Test State",
-                                    totalLotAssigned: 0,
-                                }
-                            ]
-                        }
-                    })
-                ) :
-                res(
-                    ctx.status(200),
-                    ctx.json({
+                    }
+                    :
+                    {
                         data: {
                             "dspId": "111",
                             "dspName": "dsp updated1 test",
@@ -94,9 +52,9 @@ export const getDspDataHandler = () => {
                             "stateNm": "CA",
                             "postalCd": "95070",
                             "activeInactiveInd": "Y"
-                        },
-                        error: null
-                    })
-                );
+                        }
+                    }
+            )
+        );
     });
 };
