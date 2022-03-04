9c9dc0fc02c1e1662a63bcbe67b45b54dfdb3b5f
Nikhil Patel <nikhil.patel2@shell.com>
2022-03-04 11:14:26 +0530

    1037842: Sonar fixes

--- src/pages/DspLanding/mockApi/handlerDSPList.ts
+++ src/pages/DspLanding/mockApi/handlerDSPList.ts
@@ -4,8 +4,50 @@ export const getDSPList = () => {
     return rest.get('*/api/customer-service/customers/*/dsps*', (req, res, ctx) => {
         const search = req.url.searchParams.get('search');
         const params = req.params;
-        if (search?.trim()) {
-            return search === "Test DSP" ?
+        return (search?.trim()) ?
+            search === "Test DSP" ?
+                res(
+                    ctx.status(200),
+                    ctx.json({
+                        data: {
+                            pagination: {
+                                "totalCount": 1,
+                                "limit": 15,
+                                "offset": 0
+                            },
+                            dsps: [
+                                {
+                                    address: "12343 Bonfire Dr,  Bonfire Dr,   Bonfire Dr",
+                                    city: "Reisterstown",
+                                    contactName: "Test Contact",
+                                    customerId: "test-customer-id",
+                                    email: "d.s.h.j.s@h.dc.com",
+                                    id: "3a14f46b-0c85-441a-a532-4e068b3b6f3e",
+                                    lots: [],
+                                    name: "Test DSP",
+                                    postalCode: "21136",
+                                    state: "Test State",
+                                    totalLotAssigned: 0,
+                                }
+                            ]
+                        }
+                    })
+                ) :
+                res(
+                    ctx.status(200),
+                    ctx.json({
+                        data: {
+                            pagination: {
+                                "totalCount": 0,
+                                "limit": 15,
+                                "offset": 0
+                            },
+                            dsps: []
+                        }
+                    })
+                )
+            :
+            (params[2] !== '/222') ?
                 res(
                     ctx.status(200),
                     ctx.json({
@@ -46,36 +88,6 @@ export const getDSPList = () => {
                         }
                     })
                 );
-        } else {
-            if (params[2] !== '/222') {
-                return res(
-                    ctx.status(200),
-                    ctx.json({
-                        data: {
-                            pagination: {
-                                "totalCount": 1,
-                                "limit": 15,
-                                "offset": 0
-                            },
-                            dsps: [
-                                {
-                                    address: "12343 Bonfire Dr,  Bonfire Dr,   Bonfire Dr",
-                                    city: "Reisterstown",
-                                    contactName: "Test Contact",
-                                    customerId: "test-customer-id",
-                                    email: "d.s.h.j.s@h.dc.com",
-                                    id: "3a14f46b-0c85-441a-a532-4e068b3b6f3e",
-                                    lots: [],
-                                    name: "Test DSP",
-                                    postalCode: "21136",
-                                    state: "Test State",
-                                    totalLotAssigned: 0,
-                                }
-                            ]
-                        }
-                    })
-                );
-            }
-        }
+
     });
 };
