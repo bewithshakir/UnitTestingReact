95a2d13b788f72285d80845ea84a09b8f04c1210
Sai.S.Krishna <Sai.S.Krishna@shell.com>
2022-03-24 13:05:08 +0530

    PR Comments

--- src/pages/Users/queries.ts
+++ src/pages/Users/queries.ts
@@ -33,7 +33,7 @@ const getUsersList = async (pageParam: number,
 };
 
 
-export const usersGetListSet = (query: string, sortOrder: { sortBy: string, order: string }, customerId: string, filterParams?: { [key: string]: string[] }) => {
+export const useGetUsersList = (query: string, sortOrder: { sortBy: string, order: string }, customerId: string, filterParams?: { [key: string]: string[] }) => {
     return useInfiniteQuery(["getUsersList", query, 
                                 sortOrder, 
                                 filterParams, 
