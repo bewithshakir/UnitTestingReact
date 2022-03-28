457a03fccaebe99132fc4f807a25e5801668db31
Sai.S.Krishna <Sai.S.Krishna@shell.com>
2022-03-28 13:51:27 +0530

    sonar qube

--- src/pages/Users/queries.ts
+++ src/pages/Users/queries.ts
@@ -23,9 +23,9 @@ const getUsersList = async (pageParam: number,
         }
     }
 
-    const usersGetListEntitySet = `/api/user-service/users?limit=${pageDataLimit}&offset=${pageParam}&customerId=fdd19f8e-7862-4875-9b11-73d022a6e025`;
-    const querySplit = `&countryCode=us${query.toString().length ? `&${query.toString()}` : ''}`;
-    const url = query ? querySplit : `&countryCode=us`;
+    const usersGetListEntitySet = `/api/user-service/users?limit=${pageDataLimit}&offset=${pageParam}&customerId=${customerId}`;
+
+    const url = query ? `&countryCode=us${query.toString().length ? `&${query.toString()}` : ''}` : `&countryCode=us`;
 
     const options: AxiosRequestConfig = {
         method: 'get',
